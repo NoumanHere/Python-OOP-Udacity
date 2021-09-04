bc0e856971c175339a03904a19bf0078c61d3c65
Muhammad Nouman <59723761+NoumanHere@users.noreply.github.com>
2021-09-04 09:31:22 +0500

    Magic Methods - Inheritence - PyPi Package .

---
+++ OOP/3a_python_package/distributions/__init__.py
@@ -0,0 +1,1 @@
+from .Gaussiondistribution import Gaussian
---
+++ OOP/3a_python_package/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name = 'distributions',
+	version = '0.0.1',
+	description = 'Gaussion distributions',
+	packages = ['distributions']
+	
+	)
